105caa80a7d13393640145e20935d95092e356b6
Jon Turner <jon.turner@janrain.com>
2021-10-11 12:30:50 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 janrain.settings.capture = janrain.settings.capture || {}
 janrain.settings.capture.beforeJanrainCaptureWidgetOnLoad = janrain.settings.capture.beforeJanrainCaptureWidgetOnLoad || []
 janrain.settings.capture.beforeJanrainCaptureWidgetOnLoad.push(function() {
-  console.log('Testing');
-  alert("This is test alert box");
+  // console.log('Testing');
+  // alert("This is test alert box");
+  janrain.events.onProviderLoginSuccess.addHandler(function(result) {
+    alert("onProviderLoginSuccess");
+    console.log('before');
+    console.log(event);
+    console.log('after');
+  });
+  janrain.events.onCaptureSaveSuccess.addHandler(function(result) {
+    alert("onProviderLoginSuccess");
+    console.log(result.authProvider);
+    console.log(event);
+    console.log('cool');
+  });
 });
 
 // janrain.debug.enable();
