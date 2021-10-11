677ca4c5176efd569f4cfe65548c792bc7a86c3e
Jon Turner <jon.turner@janrain.com>
2021-10-11 11:16:53 -0700

    Update test.js
    add handler for onProviderLoginSuccess event

--- test.js
+++ test.js
@@ -4,3 +4,11 @@ janrain.settings.capture.beforeJanrainCaptureWidgetOnLoad.push(function() {
   console.log('Testing');
   alert("This is test alert box");
 });
+
+function janrainCaptureWidgetOnLoad() {
+    janrain.events.onProviderLoginSuccess.addHandler(function(result) {
+        console.log('before');
+        console.log(event);
+        console.log('after');
+    });
+}
